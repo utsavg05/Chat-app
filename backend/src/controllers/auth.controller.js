56ae15311858640e1b60800478111a57cd47b0b2
utsavg05 <utsav.gp1204@gmail.com>
2025-06-09 17:49:42 +0530

    Implement messaging feature with user sidebar, message retrieval, and sending functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,12 +52,12 @@ export const login = async (req, res) => {
         const user = await User.findOne({ email })
         if (!user) return res.status(400).json({ message: 'Invalid credentials' })
 
-        const isPasswordCoorect = await bcrypt.compare(password, user.password)
-        if (!isPasswordCoorect) return res.status(400).json({ message: 'Invalid credentials' })
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' })
 
         generateToken(user._id, res)
-        res.status(201).json({
-            message: `Welcome back ${user.fullName}`,
+        res.status(200).json({
+            message: `Welcome back ${user.fullName} 😊`,
             _id: user._id,
             fullName: user.fullName,
             email: user.email,
@@ -87,15 +87,24 @@ export const updateProfile = async (req, res) => {
         if (!profilePic) return res.status(400).json({ message: "Profile pic is required" })
 
         const uploadResponse = await cloudinary.uploader.upload(profilePic)
-        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true })
 
-        res.status(200).json({
-            message: "User updated successfully",
-            updatedUser
-        })
+        res.status(200).json(updatedUser)
 
     } catch (error) {
         console.log("Error in update profile: ", error.message);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth controller: ", error.messgae);
         res.status(500).json({ message: "Internal server error" })
     }
 }
---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { connectDB } from './lib/db.js'
+
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+const PORT = process.env.PORT
+
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+
+app.listen(5001, () => {
+    console.log(`server is running on port: ${PORT}`);   
+    connectDB() 
+})
