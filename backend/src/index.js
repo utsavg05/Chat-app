e94c80c5d687b1d1788d7a48980a42f6f82290ac
utsavg05 <utsav.gp1204@gmail.com>
2025-06-10 19:28:49 +0530

    feat: integrate chatapp package and update frontend components for branding

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import { app, server } from './lib/socket.js'
 
+import path from 'path'
+
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 
@@ -21,11 +23,21 @@ app.use(cors({
 }))
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+
+}
+
 server.listen(5001, () => {
-    console.log(`server is running on port: ${PORT}`);   
-    connectDB() 
+    console.log(`server is running on port: ${PORT}`);
+    connectDB()
 })
