ea260a372ef2850bbe24f827ac5d67531394fbfd
utsavg05 <utsav.gp1204@gmail.com>
2025-06-10 22:50:07 +0530

    fix: update error message in getMessages controller for consistency

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -31,7 +31,7 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error!" });
   }
 };
 
