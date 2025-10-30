258c7a2cad7225f5e653f6531bcffaed24e1d592
utsavg05 <utsav.gp1204@gmail.com>
2025-10-30 23:44:22 +0530

    frontend host

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,18 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+//   withCredentials: true,
+// });
+
+
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://zync-backend-ruddy.vercel.app/api",
   withCredentials: true,
 });
