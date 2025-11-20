b765236dba695393958ba765367755c8fba0d174
utsavg05 <utsav.gp1204@gmail.com>
2025-11-20 11:51:41 +0530

    Update cookie settings in generateToken function for improved security

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,8 +9,9 @@ export const generateToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        samesite: "strict",
-        secure: process.env.NODE_ENV != 'development'
+        sameSite: "none",
+        // secure: process.env.NODE_ENV != 'development'
+        secure: true
     })
 
     return token
