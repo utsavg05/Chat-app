6104bb338428becd16ae1c5a8af99d2b77fa9595
utsavg05 <utsav.gp1204@gmail.com>
2025-11-20 11:39:32 +0530

    Update CORS configuration to allow requests from production URL

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",
+      "https://zync-three.vercel.app/"
+    ],
+    credentials: true
   },
 });
 
